d30771032c6c79aeed5d05c00e0205561a1ce3c7
Naga Venkata Raghuveer Chebrolu <raghuchnv@gmail.com>
2023-12-12 23:50:11 +0530

    Selenium Alerts and Waits and Assertions

--- src/test/java/com/selenium/ValidateAlerts.java
+++ src/test/java/com/selenium/ValidateAlerts.java
@@ -3,139 +3,122 @@ package com.selenium;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-import com.POM.AlertsPOM;
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.utility.Library;
+
+import io.opentelemetry.exporter.logging.SystemOutLogRecordExporter;
 
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 
+import static org.testng.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
+import java.util.Properties;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.ITestListener;
-import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.AfterSuite;
 
-public class ValidateAlerts extends Library{
-	
-	public static ExtentHtmlReporter ExtHtmlReptr;
-	public static ExtentReports ExtReports;
-	public static ExtentTest ExtTest;
-	
-  @Test(priority=-3)
-  public void LaunchToolsQAApplicationAlertsPage() {
-	  System.out.println("inside LaunchToolsQAApplicationAlertsPage");
-	  ExtTest=ExtReports.createTest(new Object() {}.getClass().getEnclosingMethod().getName());
-	  driver.get(objProperties.getProperty("AlertsUrl"));
-	  String title = driver.getTitle();
-	  Assert.assertEquals(title, objProperties.getProperty("AlertPageTitle"));
-  }
-  
-  @Test(priority=-1)
-  public void ValidateNormalAlert() {
+public class ValidateAlerts extends Library {
+
+  @Test (priority=1)
+  public void ValidateNormalAlert() throws InterruptedException {
 	  System.out.println("inside ValidateNormalAlert");
-	  ExtTest=ExtReports.createTest(new Object() {}.getClass().getEnclosingMethod().getName());
-	  //driver.findElement(By.id("alertButton")).click();
-	  AlertsPOM objAlertspom = new AlertsPOM(driver);
-	  objAlertspom.NormalAlert.click();
+	  driver.get(objProperties.getProperty("AlertsUrl"));
+	  driver.findElement(By.id("alertButton")).click();
 	  Alert objAlert = driver.switchTo().alert();
-	  String  title = objAlert.getText();
-	  Assert.assertEquals(title, objProperties.getProperty("NormalAlertTitle"));
+	  Thread.sleep(3000);
+	  String TextOfAlert = objAlert.getText();
+	  Assert.assertEquals(TextOfAlert, "You clicked a button");
 	  objAlert.accept();
   }
-  
-  @Test(priority=0)
+
+  @Test(priority=2)
   public void ValidateTimerAlert() {
 	  System.out.println("inside ValidateTimerAlert");
-	  ExtTest=ExtReports.createTest(new Object() {}.getClass().getEnclosingMethod().getName());
-	  AlertsPOM objAlertspom = new AlertsPOM(driver);
-	  objAlertspom.TimerAlert.click();
-	  
-	  //Explicit Wait : Applicable for one webElement for a specified time duration based on the condition
+	  driver.findElement(By.id("timerAlertButton")).click();
+	  //Explicit Wait : Applicable for only one specific web element.
+	  //It will wait until expected condition is satisfied for a specified time duration.
 	  WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
 	  wait.until(ExpectedConditions.alertIsPresent());
-	  
 	  Alert objAlert = driver.switchTo().alert();
-	  String  title = objAlert.getText();
-	  Assert.assertEquals(title, objProperties.getProperty("TimerAlertTitle"));
+	  String TextOfTimerAlert = objAlert.getText();
+	  Assert.assertEquals(TextOfTimerAlert, "This alert appeared after 5 seconds");
 	  objAlert.accept();
-	  
-  }
-  
-  @Test(priority=1,dependsOnMethods= {"LaunchToolsQAApplicationAlertsPage"})
-  public void ValidateConfirmBoxAlert() {
-	  System.out.println("inside ValidateConfirmBoxAlert");
-	  ExtTest=ExtReports.createTest(new Object() {}.getClass().getEnclosingMethod().getName());
-	  AlertsPOM objAlertsPOM = new AlertsPOM(driver);
-	  objAlertsPOM.ConfirmALert.click();
-	  Alert objAlert = driver.switchTo().alert();
-	  SoftAssert objSA = new SoftAssert();
-	  //Assert.assertEquals(objAlert.getText(), objProperties.getProperty("ConfirmBoxAlertTitle"));
-	  objSA.assertEquals(objAlert.getText(), objProperties.getProperty("ConfirmBoxAlertTitle"));
-	  objAlert.dismiss();
-	  String ConfirmAlertMessage = objAlertsPOM.ConfirmResult.getText();
-	  Assert.assertEquals(ConfirmAlertMessage, objProperties.getProperty("ConfimrBoxAlertMessage"));
-	  objSA.assertAll();
-  }
-  
-  @Test(priority=2)
-  public void ValidatePromptBoxAlert() {
-	  System.out.println("inside ValidatePromptBoxAlert");
-	  ExtTest=ExtReports.createTest(new Object() {}.getClass().getEnclosingMethod().getName());
-	  AlertsPOM objAlertsPOM = new AlertsPOM(driver);
-	  objAlertsPOM.PromptAlert.click();
-	  Alert objAlert = driver.switchTo().alert();
-	  Assert.assertEquals(objAlert.getText(),objProperties.getProperty("PromptAlertMessage"));
-	  objAlert.sendKeys(objProperties.getProperty("PromptAlertText"));
-	  objAlert.accept();
-	  String PromptBoxMessage = objAlertsPOM.promptResult.getText();
-	  Assert.assertEquals(PromptBoxMessage, objProperties.getProperty("PromptAlertResult"));
 
   }
-  
+
+  @Test(priority=3)
+  public void ValidateConfirmBoxAlert() {
+	  System.out.println("inside ValidateConfirmBoxAlert");
+	  driver.findElement(By.id("confirmButton")).click();
+	  Alert objAlert = driver.switchTo().alert();
+	  String TextOfConfirmAlert = objAlert.getText();
+	  SoftAssert SAssert = new SoftAssert();
+	  SAssert.assertEquals(TextOfConfirmAlert, "Do you confirm actio");
+	 // Assert.assertEquals(TextOfConfirmAlert, "Do you confirm action?");
+	  objAlert.dismiss();
+	  String ResultOfConfirmAlert = driver.findElement(By.xpath("//span[@id='confirmResult']")).getText();
+	  Assert.assertEquals(ResultOfConfirmAlert, "You selected Cancel");
+	  SAssert.assertAll();//should be the last line in the test case
+  }
+
+  @Test (priority=4)
+  public void ValidatePromptBoxAlert() {
+	  System.out.println("inside ValidatePromptBoxAlert");
+	  driver.findElement(By.id("promtButton")).click();
+	  Alert objAlert = driver.switchTo().alert();
+	  objAlert.sendKeys("Hi How are you");
+	  objAlert.accept();
+	  String ResultOfPromptAlert = driver.findElement(By.xpath("//span[@id='promptResult']")).getText();
+	  Assert.assertEquals(ResultOfPromptAlert, "You entered Hi How are you");
+
+  }
+
+
   @BeforeMethod
   public void beforeMethod() {
 	  System.out.println("inside beforeMethod");
   }
 
   @AfterMethod
-  public void afterMethod(ITestResult result) throws IOException {
+  public void afterMethod() {
 	  System.out.println("inside afterMethod");
-	  UpdatingResultInExtentReport(result);
   }
 
   @BeforeClass
   public void beforeClass() {
 	  System.out.println("inside beforeClass");
-	  StartExtentReport();
   }
 
   @AfterClass
   public void afterClass() {
 	  System.out.println("inside afterClass");
+	 // driver.close();//close the browser that is currently active by web driver
   }
 
   @BeforeTest
   public void beforeTest() {
 	  System.out.println("inside beforeTest");
+	  System.out.println(objProperties.getProperty("application"));
 	  LaunchBroswer();
+
   }
 
-  @AfterTest
+
+@AfterTest
   public void afterTest() {
 	  System.out.println("inside afterTest");
   }
@@ -143,13 +126,13 @@ public class ValidateAlerts extends Library{
   @BeforeSuite
   public void beforeSuite() throws IOException {
 	  System.out.println("inside beforeSuite");
+	  System.out.println(System.getProperty("user.dir"));
 	  ReadPropetiesFile();
   }
 
   @AfterSuite
   public void afterSuite() {
 	  System.out.println("inside afterSuite");
-	  ExtReports.flush();
   }
 
 }
